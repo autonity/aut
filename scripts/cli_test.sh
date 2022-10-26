cd6f393299d261e601b4fafbc473401f26e35471
Duncan Tebbs <duncan.tebbs@gmail.com>
2022-10-26 23:22:38 +0100

    fix up cli_test

--- scripts/cli_test.sh
+++ scripts/cli_test.sh
@@ -3,27 +3,31 @@
 set -x
 set -e
 
-# Dummy keys:
-#
-# 00..01: '0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf'
-# 00..02: '0x2B5AD5c4795c026514f8317c7a215E218DcCD6cF'
-# 00..03: '0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69'
+# Test assumes a live testnet, and relies on Alice having some funds.
+# Got to: https://faucet.autonity.org/ to recharge.
+export WEB3_ENDPOINT="https://rpc1.piccadilly.autonity.org:8545/"
 
+# We use these dummy Dummy keys:
 ALICE=0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf
 BOB=0x2B5AD5c4795c026514f8317c7a215E218DcCD6cF
 CAROL=0x6813Eb9362372EEF6200f3b1dbC3f819671cBA69
 
-export WEB3_ENDPOINT="https://rpc1.piccadilly.autonity.org:8545/"
-
-# TODO: Get Alice's balance
+# TODO: Get Alice and Bob's balances
 # aut balance ${ALICE}
 
 # Tiny send tx from 1 to 2
-aut maketx --from ${ALICE} --to ${BOB} --value '1kwei' \
-  --gas 3000000 --gas-price 10000000000 > test.tx
+aut maketx \
+    --from ${ALICE} \
+    --to ${BOB} \
+    --value '1wei' \
+    --gas 3000000 --gas-price 1000000000 > test.tx
 
 # Sign the tx using Alices private key
 KEYFILEPWD=alice aut signtx --key-file tests/data/alice.key test.tx > test.signed.tx
 
 # Send the transaction
 aut sendtx test.signed.tx
+
+# TODO: Wait for transaction
+
+# TODO: check Alice and Bob's new balances
