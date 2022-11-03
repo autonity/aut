1dcf3b189a73b3e1ba45c81ef1834191f0c0e923
Duncan Tebbs <duncan.tebbs@gmail.com>
2022-11-03 10:58:22 +0000

    trivial draft logging infrastructure

--- autcli/__main__.py
+++ autcli/__main__.py
@@ -3,15 +3,20 @@ Autonity RPC Client
 """
 
 from autcli.commands import maketx, signtx, sendtx, waittx, get, list as list_group
+from autcli.logging import enable_logging
 
-from click import group
+from click import group, option
 
 
 @group()
-def aut() -> None:
+@option("--verbose", "-v", is_flag=True, help="Enable additional output (to stderr)")
+def aut(verbose: bool) -> None:
     """
     Command line interface to Autonity functionality.
     """
+
+    if verbose:
+        enable_logging()
 
 
 aut.add_command(maketx.maketx)
