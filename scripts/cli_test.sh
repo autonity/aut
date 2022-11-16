f4a3f71c28233cbe0c87dfba6858dc9d34cf17e6
Duncan Tebbs <duncan.tebbs@gmail.com>
2022-11-16 13:42:41 +0000

    create tx subcommand with make, sign, send, wait operations

--- scripts/cli_test.sh
+++ scripts/cli_test.sh
@@ -24,24 +24,24 @@ pushd _test_data
     mkdir -p keystore
     cp ../tests/data/alice.key keystore
     cp ../tests/data/bob.key keystore
-    aut list accounts --keystore keystore > accounts
+    aut account list --keystore keystore > accounts
     [ 2 == $(wc -l < accounts) ] || (echo "unexpected number of accounts"; exit 1)
 
     # List account info
-    aut get account --stdin < accounts
+    aut account info $(cat accounts)
 
     # Remove WEB3_ENDPOINT env var and set up a configuration file instead
     unset WEB3_ENDPOINT
     ../scripts/setup_config_file.sh
 
     # Get Alice and Bob's balances
-    alice_balance_orig=`aut get balance $ALICE`
-    bob_balance_orig=`aut get balance $BOB`
+    alice_balance_orig=`aut account balance $ALICE`
+    bob_balance_orig=`aut account balance $BOB`
 
     # Fake NTN transfer transaction.  Specify everything, to avoid
     # web3.py querying the node (which fails if ALICE has insufficient
     # NTN).
-    aut --verbose maketx \
+    aut --verbose tx make \
         --ntn \
         --from ${BOB} \
         --to ${ALICE} \
@@ -51,28 +51,28 @@ pushd _test_data
         --nonce 12 > test_ntn_tx
 
     # Sign the tx using Bobs private key
-    KEYFILEPWD=bob aut --verbose signtx --key-file keystore/bob.key test_ntn_tx > test_ntn_tx.signed
+    KEYFILEPWD=bob aut --verbose tx sign --key-file keystore/bob.key test_ntn_tx > test_ntn_tx.signed
 
     # TODO: Send the above when we can fund dummy accounts with NTN
 
     # Tiny send tx from Alice to Bob.  Extract Alice's address from
     # the keyfile given in .autrc
-    aut --verbose maketx \
+    aut --verbose tx make \
         --to ${BOB} \
         --value '0.001kwei' \
         --gas-price 1000000000 > test_tx
 
     # Sign the tx using Alices private key (taken from the .autrc file)
-    KEYFILEPWD=alice aut --verbose signtx test_tx > test_tx.signed
+    KEYFILEPWD=alice aut --verbose tx sign test_tx > test_tx.signed
 
     # Send the transaction
-    aut sendtx test_tx.signed > test_tx.hash
+    aut tx send test_tx.signed > test_tx.hash
 
     # Wait for transaction
-    aut waittx `cat test_tx.hash`
+    aut tx wait `cat test_tx.hash`
 
     # Check Bob's new balances is 1 greater than the starting balance
-    bob_balance_new=`aut get balance $BOB`
+    bob_balance_new=`aut account balance $BOB`
     [ "${bob_balance_new}" == "$((${bob_balance_orig} + 1))" ] || (
         echo "Unexpected balance change"; exit 1)
 
