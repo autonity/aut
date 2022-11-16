605aac59be1b11734be510b4b5f465388a44bba1
Duncan Tebbs <duncan.tebbs@gmail.com>
2022-11-16 18:56:23 +0000

    Abbreviated flag -t for --token

--- autcli/options.py
+++ autcli/options.py
@@ -60,7 +60,10 @@ def newton_or_token_option(fn: Func) -> Func:
     """
     fn = option("--ntn", is_flag=True, help="Use Newton (NTN) instead of Auton")(fn)
     fn = option(
-        "--token", metavar="TOKEN-ADDR", help="Use the ERC20 token at the given address"
+        "--token",
+        "-t",
+        metavar="TOKEN-ADDR",
+        help="Use the ERC20 token at the given address",
     )(fn)
     return fn
 
